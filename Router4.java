e9d3471320ddec16036b298cd754bcf3efd51dde
joperafe <joaoferreira.miecom@gmail.com>
2018-04-23 09:19:08 +0100

    Update Router4.java

--- Router4.java
+++ Router4.java
@@ -3,11 +3,62 @@ package ndn;
 
 import java.net.*;
 import java.util.*;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
 /**
  *
  * @author andre
  */
 public class Router4 {
+        static final class vars {
+        private final int fifo;
+        private final int ttl;
+        public vars(int fifo, int ttl) {
+            this.fifo = fifo;
+            this.ttl = ttl;
+        }
+        public int getFifo() {
+            return fifo;
+        }
+        public int getTTL() {
+            return ttl;
+        }
+    }
+    public static vars configfiles(){
+        int fifo = 0;
+        int ttl = 0;
+        try {
+            File fXmlFile = new File("C:/Users/Asus/Desktop/XML/configfile.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+            //optional, but recommended
+            //read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+            doc.getDocumentElement().normalize();
+            //System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+            NodeList nList = doc.getElementsByTagName("lista");
+            //System.out.println("----------------------------");
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                //System.out.println("#########################");
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    fifo = Integer.parseInt(eElement.getElementsByTagName("fifo").item(0).getTextContent());
+                    ttl = Integer.parseInt(eElement.getElementsByTagName("ttl").item(0).getTextContent());
+                    //System.out.println("Conteudo: " + fifo);
+                    //System.out.println("Conteudo: " + eElement.getElementsByTagName("ttl").item(0).getTextContent());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new vars(fifo, ttl);
+    }
         
     public static List<InetAddress> obtainValidAddresses(InetAddress group) {
       List<InetAddress> result = new ArrayList<InetAddress>();
@@ -50,7 +101,10 @@ public class Router4 {
 
    public static void main(String[] args) throws Exception 
    {
-       LinkedHashMap<String,String> cs = new LinkedHashMap<String,String>(3, (float) 0.75, true);
+       var variables = configfiles();
+       int fifo = variables.getFifo();
+       int ttl = variables.getTTL();
+       LinkedHashMap<String,String> cs = new LinkedHashMap<String,String>(fifo, (float) 0.75, true);
        Map<String, List<String>> pit = new HashMap<String, List<String>>(); 
        
       //InetAddress group = InetAddress.getByName("FF15::1:2:3");
