67c998d39d856699440a5fcfc270f98888986c34
joperafe <joaoferreira.miecom@gmail.com>
2018-05-30 02:31:44 +0100

    Update ReadURLXMLFile.java

--- Bigodes/ReadURLXMLFile.java
+++ Bigodes/ReadURLXMLFile.java
@@ -1,15 +1,21 @@
 package xml;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import hashmap.United;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
 public class ReadURLXMLFile {
     //-------FIFO e TTL--------
@@ -78,18 +84,32 @@ public class ReadURLXMLFile {
         ReadURLXMLFile.vars variables = configfiles();
         // URL deve ser passado no ficheiro de configuração
         //String url = "http://www.dnoticias.pt/rss/pais.xml";
-        String urlPais = variables.getPais();
-        String urlMundo = variables.getMundo();
-        String urlDesporto = variables.getDesporto();
-        // Só para as noticias de desporto de momento
+        String DNurlPais = variables.getPais();
+        String DNurlMundo = variables.getMundo();
+        String DNurlDesporto = variables.getDesporto();
+
+        criaxml("dnpais", DNurlPais);
+        criaxml("dndesporto", DNurlDesporto);
+        criaxml("dnmundo", DNurlMundo);
+    }
+
+    public static void criaxml(String tema, String url) throws ParserConfigurationException, IOException, SAXException, TransformerException {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
-        Document docDesporto = db.parse(new URL(urlDesporto).openStream());
+        Document data = db.parse(new URL(url).openStream());
         // Normaliza texto
-        docDesporto.getDocumentElement().normalize();
+        data.getDocumentElement().normalize();
         // Vai buscar cada separador "item"
-        NodeList nList = docDesporto.getElementsByTagName("item");
+        NodeList nList = data.getElementsByTagName("item");
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        // root elements
+        // TEMAS
+        Document doc = docBuilder.newDocument();
+        Element rootElement = doc.createElement(tema);
+        doc.appendChild(rootElement);
         // Corre as primeiras 5 noticias existentes
+
         for (int temp = 0; temp < 5; temp++) {
             Node nNode = nList.item(temp);
             if (nNode.getNodeType() == Node.ELEMENT_NODE) {
@@ -97,6 +117,23 @@ public class ReadURLXMLFile {
                 Element eElement = (Element) nNode;
                 // Vai buscar cada separador "title"
                 System.out.println("Noticia : " + eElement.getElementsByTagName("title").item(0).getTextContent());
+                // Cria data.xml
+                Element staff = doc.createElement(tema);
+                rootElement.appendChild(staff);
+                // set id da noticia
+                staff.setAttribute("id", String.valueOf(temp));
+                staff.appendChild(doc.createTextNode(eElement.getElementsByTagName("title").item(0).getTextContent()));
+                // write the content into xml file
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer();
+                DOMSource source = new DOMSource(doc);
+                StreamResult result = new StreamResult(new File("C:/Users/Asus/Desktop/"+ tema +".xml"));
+                // Output to console for testing
+                // StreamResult result = new StreamResult(System.out);
+                transformer.transform(source, result);
+                System.out.println("File saved!");
+                StreamResult result1 = new StreamResult(System.out);
+                transformer.transform(source, result1);
             }
         }
     }
